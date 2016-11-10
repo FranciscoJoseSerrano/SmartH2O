c80727e076b9ae8e0c7adc145cf24ae4b2034584
Marta Carvalho <2140258@my.ipleiria.pt>
2016-11-10 22:43:48 +0000

    inicializar dll e mostrar dados

--- SmartH2O_DU/Program.cs
+++ SmartH2O_DU/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SmartH2O_DU
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            int delay = 1000;
+            SensorNodeDll.SensorNodeDll dll = new  SensorNodeDll.SensorNodeDll();
+            dll.Initialize(readDataFromDll, delay);
+
+        }
+
+        private static void readDataFromDll(string message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
