28a43ed2b761c0d1cce05dab8eec17da1a5fcbb4
Francisco <2140252@my.ipleiria.pt>
2016-12-11 01:06:12 +0000

    cenas

--- SmartH2O_SeeApp/Form1.cs
+++ SmartH2O_SeeApp/Form1.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows.Forms;
-using System.Windows.Forms.DataVisualization.Charting;
+
 
 namespace SmartH2O_SeeApp
 {
